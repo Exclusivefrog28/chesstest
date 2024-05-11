1315cf963bc37dc78cfad0a64d5702a7eb2dbedf
excf28 <deee4444@gmail.com>
2024-05-11 16:55:38 +0200

    small refactor

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import argparse
 import random
 import subprocess
-from asyncio import create_task, run, Semaphore, get_event_loop
+from asyncio import create_task, run, Semaphore, get_event_loop, create_subprocess_exec
+from asyncio.subprocess import PIPE
 from datetime import datetime
 from math import floor
 
@@ -10,66 +11,89 @@ import chess
 
 async def play_match(match):
     def write(engine, command):
-        engine.stdin.write(command + '\n')
-        engine.stdin.flush()
+        engine.stdin.write((command + '\n').encode("utf-8"))
 
     async def read(engine, command, log=None, player=None):
-        loop = get_event_loop()
-        log.write(f"waiting for engine\n")
-        while True:
-            out = await loop.run_in_executor(None, engine.stdout.readline)
-            if log is not None:
-                log.write(f"{player}: {out[:-1]}\n")
-            # print(out)
-            if out[:-1].split(" ")[0] == command:
-                return out[:-1]
+        try:
+            log.write(f"waiting for engine\n")
+            async for line in engine.stdout:
+                decoded = str(line.decode("utf-8"))
+                if log is not None:
+                    log.write(f"{player}: {decoded.strip()}\n")
+                if decoded.strip().split(" ")[0] == command:
+                    return decoded.strip()
+        except Exception as e:
+            print(e)
 
-    engine1 = subprocess.Popen([f"./{match.player1}"], stdout=subprocess.PIPE, stdin=subprocess.PIPE,
-                               stderr=subprocess.PIPE, text=True)
-    engine2 = subprocess.Popen([f"./{match.player2}"], stdout=subprocess.PIPE, stdin=subprocess.PIPE,
-                               stderr=subprocess.PIPE, text=True)
+    async def read_stderr(engine, player=None):
+        async for line in engine.stderr:
+            decoded = str(line.decode("utf-8"))
+            print(f"{player}: {decoded.strip()}")
+
+    engine1 = await create_subprocess_exec(f"./{match.player1}", stdin=PIPE, stdout=PIPE,
+                                           stderr=PIPE)
+    engine2 = await create_subprocess_exec(f"./{match.player2}", stdin=PIPE, stdout=PIPE,
+                                           stderr=PIPE)
+
+    player1_name = match.player1.split('.')[0]
+    player2_name = match.player2.split('.')[0]
+
+    read_stderr(engine1, player1_name)
+    read_stderr(engine2, player2_name)
 
     moves = []
 
     with open(
-            f"logs/W-{match.player1.split('.')[0]}_B-{match.player2.split('.')[0]}_{datetime.now().strftime('%H-%M-%S')}_pos{match.pos_index}",
+            f"logs/W-{engine1.pid}_B-{engine2.pid}_{datetime.now().strftime('%H-%M-%S')}_pos{match.pos_index}",
             "w") as log:
         write(engine1, 'ucinewgame')
         write(engine2, 'ucinewgame')
-        # write(engine1, 'setoption name UCI_Elo value 2000')
-        # write(engine2, 'setoption name UCI_Elo value 2000')
-        # write(engine1, 'setoption name UCI_LimitStrength value true')
-        # write(engine2, 'setoption name UCI_LimitStrength value true')
+        write(engine1, 'setoption name UCI_Elo value 2000')
+        write(engine2, 'setoption name UCI_Elo value 2000')
+        write(engine1, 'setoption name UCI_LimitStrength value true')
+        write(engine2, 'setoption name UCI_LimitStrength value true')
         write(engine1, 'isready')
         write(engine2, 'isready')
 
-        await read(engine1, "readyok", log, match.player1.split('.')[0])
-        await read(engine2, "readyok", log, match.player2.split('.')[0])
+        await read(engine1, "readyok", log, player1_name)
+        await read(engine2, "readyok", log, player2_name)
 
         board = chess.Board(fen=match.position)
 
         while True:
             write(engine1, f"position fen {match.position} moves {' '.join(moves)}")
             write(engine1, f"go movetime {match.time1}")
-            # print(board.fen())
             log.write(f"Board FEN: {board.fen()}\n")
-            output = (await read(engine1, "bestmove", log, match.player1.split('.')[0])).split(" ")[1]
+            output = (await read(engine1, "bestmove", log, player1_name)).split(" ")[1]
             moves.append(output)
+            if output == 'a8a8':
+                print('nullmove', engine1.pid)
             move = chess.Move.from_uci(output)
             board.push(move)
             outcome = board.outcome(claim_draw=True)
             if outcome is not None:
+                engine1.terminate()
+                engine2.terminate()
+                log.write("match ended\n")
+                await engine1.wait()
+                await engine2.wait()
                 return outcome.winner
             write(engine2, f"position fen {match.position} moves {' '.join(moves)}")
             write(engine2, f"go movetime {match.time2}")
-            # print(board.fen())
             log.write(f"Board FEN: {board.fen()}\n")
-            output = (await read(engine2, "bestmove", log, match.player2.split('.')[0])).split(" ")[1]
+            output = (await read(engine2, "bestmove", log, player2_name)).split(" ")[1]
             moves.append(output)
+            if output == 'a8a8':
+                print('nullmove', engine2.pid)
             move = chess.Move.from_uci(output)
             board.push(move)
             outcome = board.outcome(claim_draw=True)
             if outcome is not None:
+                engine1.terminate()
+                engine2.terminate()
+                log.write("match ended\n")
+                await engine1.wait()
+                await engine2.wait()
                 return outcome.winner
 
 
@@ -86,6 +110,7 @@ class Match:
 async def worker(match, results, semaphore):
     async with semaphore:
         winner = await play_match(match)
+
         if winner is None:
             results['draw'] += 1
             print(results)
